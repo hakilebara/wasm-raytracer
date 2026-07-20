33f81ecfc61337c0c8ed8b4a36ec3a5edabddc53
hakilebara <frederic.soumare@gmail.com>
2026-07-20 15:42:35 +0200

    Configure memory import from JS to wasm

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,12 @@ pub extern "C" fn add(left: u32, right: u32) -> u32 {
     left + right
 }
 
+#[unsafe(no_mangle)]
+pub extern "C" fn shared_buffer(ptr: *mut u8, len: usize) {
+    let slice = unsafe { std::slice::from_raw_parts_mut(ptr, len) };
+    slice[0] = 42;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
